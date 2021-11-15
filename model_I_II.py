ab775024ff73cfa0ad08d9bba6bb660f11ff3bad
zizi <zad@bu.edu>
2021-11-15 10:07:53 -0500

    Update model_I_II.py

--- model_I_II.py
+++ model_I_II.py
@@ -1,3 +1,14 @@
+# Step 1: Start with the section “load dataset”:
+# -Load the dataset including the features and the label, in the format of pandas dataframe
+
+# Step 2: Go through the section “Statistical Feature Selection”
+
+# Step 3: Go through the section “implementation of the models” to implement:
+# -Full models (i.e., least parsimonious) that contain all variables selected after sStatistical Feature Selection
+# -Sparse models that contain variables selected after both Statistical Feature Selection and Recursive Feature Elimination
+# -Parsimonious models that limit recursive feature elimination to select a model with up to 15 variables
+
+
 #import modules we need
 
 import numpy as np
